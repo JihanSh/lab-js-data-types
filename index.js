44c7833583ae86ce637ba7384570135a61d86d60
Jihan Shamas <jihan.shamas@gmail.com>
2025-01-15 13:29:12 +0100

    Solved lab

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
-
 // Print out the concatenated string
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
-
-
+console.log(tongueTwister);
 
 /*******************************************
     Iteration 1.2 | Camel Tail
@@ -23,24 +22,21 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
-
 // Print the cameLtaiL-formatted string
-
-
-
+const camelTail1 = part1.slice(0, -1) + part1.slice(-1).toUpperCase();
+const camelTail2 = part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+console.log(`${camelTail1} ${camelTail2} `);
 
 /*******************************************
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
-
+const tipAmount= billTotal*0.15
+const allAmount= billTotal +tipAmount
+console.log(`${allAmount}`)
 // Calculate the tip (15% of the bill total)
 
-
 // Print out the tipAmount
-
-
-
 
 /*******************************************
     Iteration 2.2 | Generate Random Number
@@ -48,10 +44,9 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
-
 // Print the generated random number
-
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log(randomNumber);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +57,20 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+console.log("false" ,expression1) ;
 const expression2 = a || b;
-
+console.log("true",expression2) ;
 const expression3 = !a && b;
+console.log("false",expression3);
 
 const expression4 = !(a && b);
+console.log("true");
 
 const expression5 = !a || !b;
+console.log("true");
 
 const expression6 = !(a || b);
+console.log("false");
 
 const expression7 = a && a;
+console.log("true");
